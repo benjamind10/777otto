5eeb0f4f1d08f7446159275e6ec7d4a1d2edcc51
Ben <benjamind10@pm.me>
2021-08-30 22:39:31 -0400

    testing the parse from FL pick 3

--- src/components/pages/DrawGames.js
+++ src/components/pages/DrawGames.js
@@ -2,15 +2,24 @@ import React from 'react';
 import '../../App.css';
 import Results from '../Results';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 const DrawGames = ({ currentUser }) => {
-  console.log(currentUser);
+  const parse = axios
+    .get('https://www.flalottery.com/exptkt/p3.htm')
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
   return (
     <div>
       {currentUser ? (
         <div>
           <Results />
+          {parse}
         </div>
       ) : (
         <div>You are not authorized.</div>
